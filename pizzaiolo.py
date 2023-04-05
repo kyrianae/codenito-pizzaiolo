9d69c28f589d71c61eb904a9d365aa2f4265d02f
kyrianae <kyrianae@gmail.com>
2023-04-05 19:36:32 +0200

    2 services

--- pizzaiolo.py
+++ pizzaiolo.py
@@ -10,7 +10,7 @@ def hello():
 
 @app.route('/make_pizza', methods=['GET'])
 def make_pizza():
-    return "Make pizza\n"
+    return "Pizzaiolo\tMake pizza"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8082)
