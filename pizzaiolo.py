755c092a4439edd854f8de8cd5e7988a91af3783
kyrianae <kyrianae@gmail.com>
2023-04-05 20:11:42 +0200

    added date

--- pizzaiolo.py
+++ pizzaiolo.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3.8
 from flask import Flask, json, request, Response
 import random
+import datetime
 
 app = Flask("pizzaiolo")
 
+def str_now():
+    return str(datetime.datetime.now())
+
 @app.route('/hello', methods=['GET'])
 def hello():
-    print ("Pizzaiolo says: Hello World")
-    return "Pizzaiolo says: Hello World\n"
+    print (str_now()+"\t"+"Pizzaiolo says: Hello World")
+    return str_now()+"\t"+"Pizzaiolo says: Hello World\n"
 
 @app.route('/make_pizza', methods=['GET'])
 def make_pizza():
-    print ("Pizzaiolo\tMade a pizza")
-    return "Pizzaiolo\tMade a pizza"
+    print (str_now()+"\t"+"Pizzaiolo\tMade a pizza")
+    return str_now()+"\t"+"Pizzaiolo\tMade a pizza"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8082)
